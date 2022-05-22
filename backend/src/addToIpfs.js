4344efe59ab2aba0db9f75c4bf1b10abb83c4b09
brgltd <brgltdxyz@gmail.com>
2022-05-22 17:07:56 -0300

    feat: add to ipfs

---
+++ backend/src/addToIpfs.js
@@ -0,0 +1,32 @@
+const { NFTStorage, File } = require("nft.storage");
+const mime = require("mime");
+const fs = require("fs");
+const path = require("path");
+
+const token = process.env.TOKEN;
+
+async function fileFromPath(filePath) {
+  const content = fs.readFileSync(filePath);
+  const type = mime.getType(filePath);
+  return new File([content], path.basename(filePath), { type });
+}
+
+async function storeNft(imagePath, name, description) {
+  const image = await fileFromPath(imagePath);
+  const nftstorage = new NFTStorage({ token });
+  console.log("calling nftstorage.store");
+  return nftstorage.store({
+    image,
+    name,
+    description,
+  });
+}
+
+async function addToIpfs(entity) {
+  const { filename, title, description } = entity;
+  const imagePath = `./public/${filename}`;
+  const result = await storeNft(imagePath, title, description);
+  return result;
+}
+
+module.exports = addToIpfs;
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const multer = require("multer");
 const fs = require("fs");
 const generateFilename = require("./generateFilename");
+const addToIpfs = require("./addToIpfs");
 require("dotenv/config");
 
 const app = express();
@@ -16,17 +17,6 @@ app.use(
 );
 app.use(express.static("public"));
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public");
-  },
-  filename: (req, file, cb) => {
-    cb(null, generateFilename(file.originalname));
-  },
-});
-
-const upload = multer({ storage: storage }).single("file");
-
 const dir = "./public";
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
@@ -34,24 +24,38 @@ if (!fs.existsSync(dir)) {
 
 const db = [];
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage }).single("file");
+
 app.post("/image", (req, res) => {
   upload(req, res, (err) => {
     if (err) {
       res.sendStatus(500);
     }
-    const filename = generateFilename(req.file.originalname);
+    // const filename = generateFilename(req.file.originalname);
+    const filename = req.file.originalname;
     db.push({ filename });
     res.send(req.file);
   });
 });
 
-app.post("/metadata", (req, res) => {
+app.post("/metadata", async (req, res) => {
   const meta = {
     title: req.body.title,
     description: req.body.description,
     date: Date.now(),
   };
   db[db.length - 1] = { ...db[db.length - 1], ...meta };
+  const entity = db[db.length - 1];
+  await addToIpfs(entity);
   console.log("db");
   console.log(db);
 });

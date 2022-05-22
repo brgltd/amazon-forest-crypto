80f368b312192b0f25a37060257bfbfefb3d36af
brgltd <brgltdxyz@gmail.com>
2022-05-22 20:20:30 -0300

    feat: add static data on top of ipfs data

--- frontend/components/images-list/images-list.js
+++ frontend/components/images-list/images-list.js
@@ -3,11 +3,10 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import axios from "axios";
 import Item from "../images-list-item/images-list-item";
+import { staticData } from "./static";
 
 export default function ImagesList() {
   const [assets, setAssets] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -16,10 +15,7 @@ export default function ImagesList() {
         const data = res.data;
         const newData = await Promise.all(
           data.map(async (item) => {
-            console.log("item");
-            console.log(item);
             const ipnft = item?.cid;
-            console.log(ipnft);
             if (ipnft) {
               const metadataUrl = `https://${ipnft}.ipfs.dweb.link/metadata.json`;
               const metadata = await axios.get(metadataUrl);
@@ -32,12 +28,11 @@ export default function ImagesList() {
             return { ...item, imgUrl: null };
           })
         );
-        console.log(newData);
-        setLoading(false);
+        console.log(JSON.stringify(newData));
         setAssets(newData);
       } catch (error) {
-        setLoading(false);
-        setError(true);
+        console.error("error fetching data");
+        console.error(error);
       }
     }
 
@@ -48,6 +43,21 @@ export default function ImagesList() {
     <Box sx={{ paddingTop: 3, m: 3, borderTop: 1, borderColor: "divider" }}>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
+          {/*Static data with images from the forest*/}
+          {staticData.map(
+            ({ title, description, imgUrl, date, filename }, index) => (
+              <Grid item sx={12} md={6} lg={4} key={index}>
+                <Item
+                  title={title}
+                  description={description}
+                  imgUrl={imgUrl}
+                  date={date}
+                  filename={filename}
+                />
+              </Grid>
+            )
+          )}
+          {/* Real world data coming from IPFS */}
           {assets.map(
             ({ title, description, imgUrl, date, filename }, index) => (
               <Grid item sx={12} md={6} lg={4} key={index}>
@@ -61,95 +71,6 @@ export default function ImagesList() {
               </Grid>
             )
           )}
-        </Grid>
-      </Box>
-    </Box>
-  );
-  return (
-    <Box sx={{ paddingTop: 3, m: 3, borderTop: 1, borderColor: "divider" }}>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-        </Grid>
-      </Box>
-    </Box>
-  );
-
-  return (
-    <Box sx={{ paddingTop: 3, m: 3, borderTop: 1, borderColor: "divider" }}>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
-          <Grid item xs={12} md={6} lg={4}>
-            <Item />
-          </Grid>
         </Grid>
       </Box>
     </Box>
---
+++ frontend/components/images-list/static.js
@@ -0,0 +1,31 @@
+// Render the first three images statically
+// To render something just in case the APIs are down.
+export const staticData = [
+  {
+    filename: "amazon2.jpg",
+    title: "High",
+    description:
+      "The rainforest likely formed during the Eocene era (from 56 million years to 33.9 million years ago). It appeared following a global reduction of tropical temperatures when the Atlantic Ocean had widened sufficiently to provide a warm, moist climate to the Amazon basin.",
+    date: 1653260049620,
+    cid: "bafyreifo2njocf5oyiaehgv6m26tz3cdikbcz5dip24xh2kakd3jgmunte",
+    imgUrl: "/amazon2.jpg",
+  },
+  {
+    filename: "amazon2.jpg",
+    title: "Swamp",
+    description:
+      "There is evidence that there have been significant changes in the Amazon rainforest vegetation over the last 21,000 years through the last glacial maximum (LGM) and subsequent deglaciation.",
+    date: 1653260049620,
+    cid: "bafyreifo2njocf5oyiaehgv6m26tz3cdikbcz5dip24xh2kakd3jgmunte",
+    imgUrl: "/amazon3.jpg",
+  },
+  {
+    filename: "amazon1.jpg",
+    title: "Water and Trees",
+    description:
+      "The Amazon rainforest, alternatively, the Amazon jungle[a] or Amazonia, is a moist broadleaf tropical rainforest in the Amazon biome that covers most of the Amazon basin of South America.",
+    date: 1653257432204,
+    cid: "bafyreigm6r62zinetirlqiiqeavibdotcwu3slgwualhljbkwvfhhmgbki",
+    imgUrl: "/amazon1.jpg",
+  },
+];
